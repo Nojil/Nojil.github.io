1a6eee3f92eefe90828c0288f941cc043a76dab8
Charles <clang676@gmail.com>
2018-05-10 21:05:13 -0600

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+$(function () {
+    $(".typed").typed({
+        strings: ["a Web Developer.", "a Teacher.", "a entrepreneur.", "a Designer.", "a UI / UI Engineer."],
+        typeSpeed: 0,
+        loop: true
+    });
+}); 
