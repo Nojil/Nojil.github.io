77fb64fd7f8b8fdc3bdc98e12585a8a1c58797f3
Charles <clang676@gmail.com>
2018-05-12 15:00:39 -0600

    Spelling corrections

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 $(function () {
     $(".typed").typed({
-        strings: ["a Web Developer.", "a Teacher.", "a entrepreneur.", "a Designer.", "a UI / UI Engineer."],
+        strings: ["a Web Developer.", "a Teacher.", "an Entrepreneur.", "a Designer.", "a UI / UI Engineer."],
         typeSpeed: 0,
         loop: true
     });
