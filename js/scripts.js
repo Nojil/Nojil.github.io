7dedc2ab3ef9a2adc0e3d8be3bbdde4f5898c88d
Charles <clang676@gmail.com>
2018-05-12 15:42:08 -0600

    Further cleanup and added curriculum page

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,28 @@ $(function () {
         loop: true
     });
 }); 
+
+/// smooth scroll
+$('.scrolling').on('click', function (event) {
+    var $anchor = $(this);
+    $('html, body').stop().animate({
+        scrollTop: $($anchor.attr('href')).offset().top
+    }, 500);
+    event.preventDefault();
+});
+
+var doc_height = $(document).height();
+$(window).on('scroll', function () {
+    if ($(this).scrollTop() > (doc_height - 1000)) {
+        $('.scroll-top.active').removeClass('active');
+        $('.scroll-top').addClass('active');
+    } else {
+        $('.scroll-top').removeClass('active');
+    }
+});
+$('.scroll-top').on('click', function () {
+    $("html, body").animate({
+        scrollTop: 0
+    }, 600);
+    return false;
+});
