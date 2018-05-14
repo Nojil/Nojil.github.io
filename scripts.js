ccdc780808b349cb539d3ac49c0a32918631a4bf
Charles Lang <clang676@gmail.com>
2018-05-14 09:08:09 -0600

    General Updates

---
+++ scripts.js
@@ -0,0 +1,36 @@
+$(function () {
+    $(".typed").typed({
+        strings: ["a Web Developer.", "a Teacher.", "an Entrepreneur.", "a Designer.", "a UI / UI Engineer."],
+        typeSpeed: 0,
+        loop: true
+    });
+});
+
+/// smooth scroll
+$('.scrolling').on('click', function (event) {
+    var $anchor = $(this);
+    $('html, body').stop().animate({
+        scrollTop: $($anchor.attr('href')).offset().top
+    }, 500);
+    event.preventDefault();
+});
+
+var doc_height = $(document).height();
+$(window).on('scroll', function () {
+    if ($(this).scrollTop() > (doc_height - 1000)) {
+        $('.scroll-top.active').removeClass('active');
+        $('.scroll-top').addClass('active');
+    } else {
+        $('.scroll-top').removeClass('active');
+    }
+});
+$('.scroll-top').on('click', function () {
+    $("html, body").animate({
+        scrollTop: 0
+    }, 600);
+    return false;
+});
+
+$('.sm-nav-toggler').on('click', function () {
+    $('.floating-menu').toggleClass('nav-open');
+});
